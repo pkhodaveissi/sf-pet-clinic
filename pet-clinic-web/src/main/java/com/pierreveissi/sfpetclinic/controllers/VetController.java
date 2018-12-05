0cb5c9d6d46cf42778fa7270c138fabc513089bd
Pierre Veissi <pooria.khodaveissi@gmail.com>
2018-12-05 18:15:23 +0330

    * dev: implement vets index page & controller closes #7

---
+++ pet-clinic-web/src/main/java/com/pierreveissi/sfpetclinic/controllers/VetController.java
@@ -0,0 +1,12 @@
+package com.pierreveissi.sfpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class VetController {
+  @GetMapping({"vets","vets/", "vets/index", "vets/index.html"})
+  public String listVets(){
+    return "vets/index";
+  }
+}
