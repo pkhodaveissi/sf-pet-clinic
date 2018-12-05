1485f754697b0e8760f9d548854eed1de8cd0ea6
Pierre Veissi <pooria.khodaveissi@gmail.com>
2018-12-05 17:56:41 +0330

    * dev: implement index page & controller closes #8

---
+++ pet-clinic-web/src/main/java/com/pierreveissi/sfpetclinic/controllers/IndexController.java
@@ -0,0 +1,12 @@
+package com.pierreveissi.sfpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+  @GetMapping({"","/", "index", "index.html"})
+  public String index(){
+    return "index";
+  }
+}
