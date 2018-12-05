76863e4c0669f6256c12589f5bab42016612f498
Pierre Veissi <pooria.khodaveissi@gmail.com>
2018-12-05 16:21:15 +0330

    * dev: added BaseEntity Class closes #10

--- pet-clinic-data/src/main/java/com/pierreveissi/sfpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/pierreveissi/sfpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.pierreveissi.sfpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
   private String name;
   private PetType petType;
   private Owner owner;
