3436d421577950ef09fcc966313ec0a5af5d0c81
Pierre Veissi <pooria.khodaveissi@gmail.com>
2018-12-05 07:11:00 +0330

    Initial Project

---
+++ src/main/java/com/pierreveissi/sfpetclinic/SfPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.pierreveissi.sfpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfPetClinicApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SfPetClinicApplication.class, args);
+  }
+}
