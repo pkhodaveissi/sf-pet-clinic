6253ab100846405ea56fe2716d5f2376b7824d4f
Pierre Veissi <pooria.khodaveissi@gmail.com>
2018-12-08 01:32:50 +0330

    * dev: lists vets via thymeleaf now   ** update related controller closes #15
    Signed-off-by: Pierre Veissi <pooria.khodaveissi@gmail.com>

--- pet-clinic-web/src/main/java/com/pierreveissi/sfpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/pierreveissi/sfpetclinic/controllers/VetController.java
@@ -1,14 +1,23 @@
 package com.pierreveissi.sfpetclinic.controllers;
 
+import com.pierreveissi.sfpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("vets")
 @Controller
 public class VetController {
+  private final VetService vetService;
+
+  public VetController(VetService vetService) {
+    this.vetService = vetService;
+  }
+
   @GetMapping({"","/", "/index", "/index.html"})
-  public String listVets(){
+  public String listVets(Model model){
+    model.addAttribute("vets", vetService.findAll());
     return "vets/index";
   }
 }
