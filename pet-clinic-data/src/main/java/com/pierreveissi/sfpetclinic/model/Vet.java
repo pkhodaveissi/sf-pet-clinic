44a4cdfbf76236f9df647b0a2b3b2eba9446aa54
Pierre Veissi <pooria.khodaveissi@gmail.com>
2018-12-08 19:30:30 +0330

    * dev: added Specialty Entity Class     ** associates Specialty with Vet Entity
    closes #27
    
    Signed-off-by: Pierre Veissi <pooria.khodaveissi@gmail.com>

--- pet-clinic-data/src/main/java/com/pierreveissi/sfpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/pierreveissi/sfpetclinic/model/Vet.java
@@ -1,5 +1,15 @@
 package com.pierreveissi.sfpetclinic.model;
 
-public class Vet extends Person {
+import java.util.Set;
 
+public class Vet extends Person {
+  private Set<Specialty> specialtySet;
+
+  public Set<Specialty> getSpecialtySet() {
+    return specialtySet;
+  }
+
+  public void setSpecialtySet(Set<Specialty> specialtySet) {
+    this.specialtySet = specialtySet;
+  }
 }
