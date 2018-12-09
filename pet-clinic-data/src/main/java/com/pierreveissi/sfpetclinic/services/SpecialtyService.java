03c1438247d85290073018d965ff38b493e66f28
Pierre Veissi <pooria.khodaveissi@gmail.com>
2018-12-09 13:22:02 +0330

    * dev: adds Specialty Map Service Implementation closes #31
    Signed-off-by: Pierre Veissi <pooria.khodaveissi@gmail.com>

---
+++ pet-clinic-data/src/main/java/com/pierreveissi/sfpetclinic/services/SpecialtyService.java
@@ -0,0 +1,7 @@
+package com.pierreveissi.sfpetclinic.services;
+
+import com.pierreveissi.sfpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+
+}
