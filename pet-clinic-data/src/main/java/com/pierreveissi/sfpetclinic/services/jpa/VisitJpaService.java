e5baa751dbbc6ddfe31a64a035ef0f6672a20c22
Pierre Veissi <pooria.khodaveissi@gmail.com>
2018-12-10 13:38:49 +0330

    * dev: adds Visit Jpa Service closes #47
    Signed-off-by: Pierre Veissi <pooria.khodaveissi@gmail.com>

---
+++ pet-clinic-data/src/main/java/com/pierreveissi/sfpetclinic/services/jpa/VisitJpaService.java
@@ -0,0 +1,49 @@
+package com.pierreveissi.sfpetclinic.services.jpa;
+
+import com.pierreveissi.sfpetclinic.model.Visit;
+import com.pierreveissi.sfpetclinic.repositories.VisitRepo;
+import com.pierreveissi.sfpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Profile("springjpa")
+public class VisitJpaService implements VisitService {
+  private final VisitRepo visitRepo;
+
+  public VisitJpaService(VisitRepo visitRepo) {
+    this.visitRepo = visitRepo;
+  }
+
+  @Override
+  public Set<Visit> findAll() {
+    Set<Visit> visitSet = new HashSet<>();
+    visitRepo.findAll().forEach(visitSet::add);
+    return visitSet;
+  }
+
+  @Override
+  public Visit findById(Long id) {
+    Optional<Visit> optionalVisit = visitRepo.findById(id);
+    return optionalVisit.orElse(null);
+  }
+
+  @Override
+  public Visit save(Visit obj) {
+    return visitRepo.save(obj);
+  }
+
+  @Override
+  public void delete(Visit obj) {
+    visitRepo.delete(obj);
+  }
+
+  @Override
+  public void deleteById(Long id) {
+    visitRepo.deleteById(id);
+  }
+}
