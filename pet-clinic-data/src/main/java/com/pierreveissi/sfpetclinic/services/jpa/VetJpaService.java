94c3271271e46de037ae9b5409a8de34e10d0ce1
Pierre Veissi <pooria.khodaveissi@gmail.com>
2018-12-10 12:42:32 +0330

    * dev: adds Vet JPA Service closes #41
    Signed-off-by: Pierre Veissi <pooria.khodaveissi@gmail.com>

---
+++ pet-clinic-data/src/main/java/com/pierreveissi/sfpetclinic/services/jpa/VetJpaService.java
@@ -0,0 +1,50 @@
+package com.pierreveissi.sfpetclinic.services.jpa;
+
+import com.pierreveissi.sfpetclinic.model.Vet;
+import com.pierreveissi.sfpetclinic.repositories.VetRepo;
+import com.pierreveissi.sfpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Profile("springjpa")
+public class VetJpaService implements VetService {
+
+  private final VetRepo vetRepo;
+
+  public VetJpaService(VetRepo vetRepo) {
+    this.vetRepo = vetRepo;
+  }
+
+  @Override
+  public Set<Vet> findAll() {
+    Set<Vet> vetSet = new HashSet<>();
+    vetRepo.findAll().forEach(vetSet::add);
+    return vetSet;
+  }
+
+  @Override
+  public Vet findById(Long id) {
+    Optional<Vet> vetOptional = vetRepo.findById(id);
+    return vetOptional.orElse(null);
+  }
+
+  @Override
+  public Vet save(Vet obj) {
+    return vetRepo.save(obj);
+  }
+
+  @Override
+  public void delete(Vet obj) {
+    vetRepo.delete(obj);
+  }
+
+  @Override
+  public void deleteById(Long id) {
+    vetRepo.deleteById(id);
+  }
+}
